63fd12f0f3095cf3ad4f36495652ac941b8f2013
chaimovy <chaimovy@gmail.com>
2017-12-19 21:09:38 +0200

    partially implemneted bi graph

--- src/hw2/BipartiteGraph.java
+++ src/hw2/BipartiteGraph.java
@@ -1,25 +1,36 @@
 package hw2;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BipartiteGraph<T> {
-	
+	private Map<Node<T>, Map<T,Node<T>>> parents;
+	private Map<Node<T>, Map<T,Node<T>>> children;
     /**
      * @modifies this
      * @effects Creates a new graph named graphName. The graph is initially
      * 			empty.
      */
     public BipartiteGraph() {
-        // TODO: Implement this method
+        parents=new HashMap<Node<T>,Map<T,Node<T>>>();
+        children=new HashMap<Node<T>,Map<T,Node<T>>>();
         
     	
     }
 
     /**
      * @requires nodeLabel != null
-     * @modifies this
+     * @throws exception
+     * @modifies this.parents
      * @effects Adds a black node represented by the T nodeName to this.
      */
     public void addBlackNode(T nodeLabel) {
-    	// TODO: Implement this method
+    	if (parents.containsKey(nodeLabel))
+    	{
+    		return; //todo: throw exception
+    	}
+    	parents.put(new Node<T>(nodeLabel,false), new HashMap<T,Node<T>>());
     	
     	
     }
@@ -27,11 +38,16 @@ public class BipartiteGraph<T> {
     
     /**
      * @requires nodeLabel != null
-     * @modifies this
+     * @throws exception
+     * @modifies this.parents
      * @effects Adds a white node represented by the T nodeLable to this.
      */
     public void addWhiteNode(T nodeLabel) {
-    	// TODO: Implement this method
+    	if (parents.containsKey(nodeLabel))
+    	{
+    		return; //todo: throw exception
+    	}
+    	parents.put(new Node<T>(nodeLabel,true), new HashMap<T,Node<T>>());
     	
     	
     }
@@ -39,13 +55,13 @@ public class BipartiteGraph<T> {
     
     /**
      * @requires parentLabel && childLabel && edgeLabel !=null
-     * @modifies this
+     * @modifies this.parents and this.children
      * @effects Adds an edge from the node parentName to the node childName
      * 			in the graph graphName. The new edge's label is the T
      * 			edgeLabel.
      */
     public void addEdge(T parentLabel, T childLabel, T edgeLabel) {
-    	//TODO: Implement this method
+    	if ()
     	
     	
     }
@@ -54,7 +70,7 @@ public class BipartiteGraph<T> {
     /**
      * @return a space-separated list of all the black nodes
      */
-    public T listBlackNodes() {
+    public List<T> listBlackNodes() {
     	//TODO: Implement this method
     	return null;
     	
@@ -64,7 +80,7 @@ public class BipartiteGraph<T> {
     /**
      * @return a space-separated list of all the white nodes
      */
-    public T listWhiteNodes() {
+    public List<T> listWhiteNodes() {
     	//TODO: Implement this method
     	return null;
     	
@@ -76,7 +92,7 @@ public class BipartiteGraph<T> {
      * @throws exception 
      * @return a space-separated list of the children of parentLabel
      */
-    public T listChildren(T parentLabel) {
+    public List<T> listChildren(T parentLabel) {
     	//TODO: Implement this method
     	return null;
     }
@@ -88,7 +104,7 @@ public class BipartiteGraph<T> {
      * @return a space-separated list  of the parents of
      * 		   childLabel in the graph.
      */
-    public T listParents(T childLabel) {
+    public List<T> listParents(T childLabel) {
     	//TODO: Implement this method
     	return null;
     	
