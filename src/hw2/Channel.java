4b537d6ca5ede2f4460c5c1f6e9d391232b27ece
HguitarH <hanan.hazani@gmail.com>
2017-12-24 15:55:17 +0200

    create Channel and Participant classes

---
+++ src/hw2/Channel.java
@@ -0,0 +1,11 @@
+package hw2;
+
+public class Channel<T> implements Simulatable<T>{
+
+	@Override
+	public void simulate(BipartiteGraph<T> graph) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/hw2/Participant.java
@@ -0,0 +1,11 @@
+package hw2;
+
+public class Participant<T> implements Simulatable<T>{
+
+	@Override
+	public void simulate(BipartiteGraph<T> graph) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
