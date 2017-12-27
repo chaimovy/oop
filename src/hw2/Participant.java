9324c0985aa9dfa20714dcf19c8cafcce187d22e
chaimovy <chaimovy@gmail.com>
2017-12-27 22:13:22 +0200

    try to run

--- src/hw2/Participant.java
+++ src/hw2/Participant.java
@@ -1,39 +1,40 @@
 package hw2;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class Participant extends Filter<String,Transaction>{
+public class Participant extends Node<String,Transaction>{
 	private double fee;
-	
+	private Queue<Transaction> outBuffer;
 	public Participant(String label, double fee) {
 		super(label);
 		this.fee = fee;
+		outBuffer = new LinkedBlockingQueue<Transaction>();
 	}
 
-	public void addTransaction(Transaction tx)
+	public Boolean addTransaction(Transaction tx)
 	{
 		if(tx.getDest().equals(this.getLabel()))
 		{
-			storageBuffer.add(tx);
+			buffer.add(tx);
 		}
 		else
 		{
-			storageBuffer.add(new Transaction(this.getLabel(), fee));
+			buffer.add(new Transaction(this.getLabel(), fee));
 			outBuffer.add(new Transaction(tx.getDest(), tx.getValue() - fee));
 		}
+		return true;
 	}
 	
+	
 	@Override
-	public void simulate(BipartiteGraph<String> graph) {
-		for(Node<String> p : getChildren().values()) // assume only 1
-			while(outBuffer.size() != 0)
-				((Channel)p).addTransaction(outBuffer.poll());
+	public List<String> simulate(BipartiteGraph<String> graph) throws Exception 
+	{
+		return graph.listChildren(getLabel());
+			
 	}
 
-	@Override
-	public Queue<Transaction> getBuffer() {
-		return storageBuffer;
-	}
+
+
 }
