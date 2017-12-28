3f938815d165840b67efff1ced1253a3753cb103
HguitarH <hanan.hazani@gmail.com>
2017-12-28 22:28:44 +0200

    done

--- src/hw2/SimulatorTest.java
+++ src/hw2/SimulatorTest.java
@@ -1,7 +1,6 @@
 package hw2;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -21,7 +20,7 @@ public class SimulatorTest {
 		driver.addEdge("simi", "A2", "B3", "23");
 		driver.sendTransaction("simi", "A1", new Transaction("B2", 5));
 		driver.sendTransaction("simi", "A2", new Transaction("B3", 1));
-		 assertEquals("wrong contents in pipe", "5.0", driver.listContents("simi", "A1").toString());
+		assertEquals("wrong contents in pipe", "5.0", driver.listContents("simi", "A1").toString());
         driver.simulate("simi");
         //double t=driver.getParticipantBalace("simi", "B2");
         assertEquals("wrong contents in pipe", "", driver.listContents("simi", "A1").toString());
@@ -32,392 +31,61 @@ public class SimulatorTest {
     }
 
 	@Test
-	public void test1() throws Exception {
+	public void testA() throws Exception {
 		
 		SimulatorTestDriver driver = new SimulatorTestDriver();
-
-		assertTrue(driver != null);
-
+		
+		//configure simulations
 		driver.createSimulator("sim1");
+		driver.addChannel("sim1", "c1", 33);
+		driver.addChannel("sim1", "c2", 33);
+		driver.addParticipant("sim1", "p1", 2);
+		driver.addParticipant("sim1", "p2", 3);
+		driver.addParticipant("sim1", "p3", 5);
+		driver.addEdge("sim1", "p1", "c1", "11");
+		driver.addEdge("sim1", "c1", "p2", "12");
+		driver.addEdge("sim1", "p2", "c2", "22");
+		driver.addEdge("sim1", "c2", "p3", "23");
+		
 		driver.createSimulator("sim2");
-
-		// delta
-		double  delta =  0.001;
-
-		// sim1
+		driver.addChannel("sim2", "c", 44);
+		driver.addParticipant("sim2", "p1", 2);
+		driver.addParticipant("sim2", "p2", 3);
+		driver.addEdge("sim2", "p1", "c", "1");
+		driver.addEdge("sim2", "c", "p2", "2");
 		
-		// add channels
-		driver.addChannel("sim1", "ch1", 100);
-		driver.addChannel("sim1", "ch2", 100);
-
-
-		// add participants
-		driver.addParticipant("sim1", "amit", 3);
-		driver.addParticipant("sim1", "matan", 4);
-		driver.addParticipant("sim1", "shir", 0.9);
-
+		// create transaction
+		Transaction t = new Transaction("p3", 10);
+		driver.sendTransaction("sim1", "c1", t);
+		driver.sendTransaction("sim1", "c2", t);
 		
-		// sim2
-		// add channels
-		driver.addChannel("sim2", "ch1", 100);
-		driver.addChannel("sim2", "ch2", 100);
-
-		// add participants
-		driver.addParticipant("sim2", "amit", 3);
-		driver.addParticipant("sim2", "matan", 4);
-				
-		// connect channels and participants
-		
-		driver.addEdge("sim1", "amit", "ch1", "ato1");
-		driver.addEdge("sim1", "ch1", "matan", "1tom");
-		driver.addEdge("sim1", "matan", "ch2", "mto2");
-		driver.addEdge("sim1", "ch2", "shir", "2tos");
-
-		
-		//check balance
-		assertEquals(driver.getParticipantBalace("sim1", "amit") ,0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan"), 0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir"), 0,delta);
-
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-
-		
-		// create transactions
-		Transaction t1 = new Transaction("shir", 10);
-		Transaction t2 = new Transaction("shir", 10);
-
-		// send transaction
-		driver.sendTransaction("sim1", "ch1", t1);
-		driver.sendTransaction("sim1", "ch2", t2);
-		
-		assertEquals(driver.listContents("sim1", "ch1"),"10.0");
-		assertEquals(driver.listContents("sim1", "ch2"),"10.0");
+		// init validations
+		assertEquals(driver.getParticipantBalace("sim1", "p1"), 0, 0.001);
+		assertEquals(driver.getParticipantBalace("sim1", "p2"), 0, 0.001);
+		assertEquals(driver.getParticipantBalace("sim1", "p3"), 0, 0.001);
+		assertEquals(driver.listContents("sim1", "c1"), "");
+		assertEquals(driver.listContents("sim1", "c2"), "");
+		assertEquals(driver.listContents("sim1", "c1"), "10.0");
+		assertEquals(driver.listContents("sim1", "c2"), "10.0");
 
 		//simulate
 		driver.simulate("sim1");
 
-		//check balance
-		assertEquals(driver.getParticipantBalace("sim1", "amit") , 0.0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan") , 4.0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir") , 10.0,delta);
-
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"6.0");
+		// validate simulation
+		assertEquals(0.0, driver.getParticipantBalace("sim1", "p1"), 0.001);
+		assertEquals(3.0, driver.getParticipantBalace("sim1", "p2"), 0.001);
+		assertEquals(10.0, driver.getParticipantBalace("sim1", "p3"), 0.001);
+		assertEquals("", driver.listContents("sim1", "c1"));
+		assertEquals("7.0", driver.listContents("sim1", "c2"));
 		
 		//simulate
 		driver.simulate("sim1");
-		
-		//check values
-		
-		//check balance
-		
-		assertEquals(driver.getParticipantBalace("sim1", "amit") , 0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan") , 4,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir") , 16,delta);
 
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-
+		// validate simulation
+		assertEquals(0, driver.getParticipantBalace("sim1", "p1"), 0.001);
+		assertEquals(3, driver.getParticipantBalace("sim1", "p2"), 0.001);
+		assertEquals(17, driver.getParticipantBalace("sim1", "p3"), 0.001);
+		assertEquals(driver.listContents("sim1", "c1"),"");
+		assertEquals(driver.listContents("sim1", "c2"),"");
 	}
-	
-	@Test
-	public void test2() throws Exception {
-		
-		SimulatorTestDriver driver = new SimulatorTestDriver();
-
-		assertTrue(driver != null);
-
-		driver.createSimulator("sim1");
-
-		// delta
-		double  delta =  0.001;
-
-		// sim1
-		
-		// add channels
-		driver.addChannel("sim1", "ch1", 1);
-		driver.addChannel("sim1", "ch2", 1);
-
-
-		// add participants
-		driver.addParticipant("sim1", "amit", 1);
-		driver.addParticipant("sim1", "matan", 4);
-		driver.addParticipant("sim1", "shir", 1);
-
-				
-		// connect channels and participants
-		
-		driver.addEdge("sim1", "amit", "ch1", "ato1");
-		driver.addEdge("sim1", "ch1", "matan", "1tom");
-		driver.addEdge("sim1", "matan", "ch2", "mto2");
-		driver.addEdge("sim1", "ch2", "shir", "2tos");
-
-		
-		//check balance
-		assertEquals(driver.getParticipantBalace("sim1", "amit") ,0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan"), 0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir"), 0,delta);
-
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-
-		
-		// create transactions
-		Transaction t1 = new Transaction("shir", 1);
-		Transaction t2 = new Transaction("shir", 10);
-
-		// send transaction
-		driver.sendTransaction("sim1", "ch1", t1);
-		driver.sendTransaction("sim1", "ch2", t2);
-		
-		assertEquals(driver.listContents("sim1", "ch1"),"1.0");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-
-		//simulate
-		driver.simulate("sim1");
-		
-		
-		//check balance
-		assertEquals(driver.getParticipantBalace("sim1", "amit") , 0.0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan") , 4.0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir") , 0.0,delta);
-
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-		
-		//simulate
-		driver.simulate("sim1");
-		
-		//check values
-		
-		//check balance
-		
-		assertEquals(driver.getParticipantBalace("sim1", "amit") , 0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan") , 4,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir") , 0.0,delta);
-
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-
-	}
-	
-	@Test
-	public void test3() throws Exception {
-		
-		SimulatorTestDriver driver = new SimulatorTestDriver();
-
-		driver.createSimulator("sim1");
-
-		// delta
-		double  delta =  0.001;
-
-		// sim1
-		
-		// add channels
-		driver.addChannel("sim1", "ch1", 100);
-		driver.addChannel("sim1", "ch2", 100);
-		driver.addChannel("sim1", "ch3", 100);
-
-
-		// add participants
-		driver.addParticipant("sim1", "amit", 5);
-		driver.addParticipant("sim1", "matan", 3);
-		driver.addParticipant("sim1", "shir", 5);
-
-				
-		// connect channels and participants
-		
-		driver.addEdge("sim1", "amit", "ch1", "ac");
-		driver.addEdge("sim1", "matan", "ch1", "mc");
-		driver.addEdge("sim1", "ch1", "shir", "1s");
-		driver.addEdge("sim1", "ch3", "amit", "2a");
-		driver.addEdge("sim1", "ch2", "matan", "3s");
-
-
-		
-		//check balance
-		assertEquals(driver.getParticipantBalace("sim1", "amit") ,0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan"), 0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir"), 0,delta);
-
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-
-		
-		// create transactions
-		Transaction t1 = new Transaction("shir", 10);
-		Transaction t2 = new Transaction("shir", 10);
-
-		// send transaction
-		driver.sendTransaction("sim1", "ch2", t1);
-		driver.sendTransaction("sim1", "ch3", t2);
-		
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"10.0");
-		assertEquals(driver.listContents("sim1", "ch3"),"10.0");
-
-
-		//simulate
-		driver.simulate("sim1");
-		
-		
-		//check balance
-		assertEquals(driver.getParticipantBalace("sim1", "amit") , 5.0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan") , 3.0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir") , 0.0,delta);
-
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"7.0 5.0");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-		assertEquals(driver.listContents("sim1", "ch3"),"");
-
-		
-		//simulate
-		driver.simulate("sim1");
-		//check values
-		
-		//check balance
-		
-		assertEquals(driver.getParticipantBalace("sim1", "amit") , 5,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan") , 3,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir") , 12,delta);
-
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-		assertEquals(driver.listContents("sim1", "ch3"),"");
-
-
-	}
-	@Test
-	public void test4() throws Exception {
-		
-		SimulatorTestDriver driver = new SimulatorTestDriver();
-
-		driver.createSimulator("sim1");
-
-		// delta
-		double  delta =  0.001;
-
-		// sim1
-		
-		// add channels
-		driver.addChannel("sim1", "ch1", 100);
-		driver.addChannel("sim1", "ch2", 100);
-		driver.addChannel("sim1", "ch3", 100);
-		driver.addChannel("sim1", "ch4", 100);
-
-
-
-		// add participants
-		driver.addParticipant("sim1", "amit", 5);
-		driver.addParticipant("sim1", "matan", 5);
-		driver.addParticipant("sim1", "shir", 5);
-		driver.addParticipant("sim1", "natali", 5);
-
-
-				
-		// connect channels and participants
-		
-		driver.addEdge("sim1", "amit", "ch1", "ac");
-		driver.addEdge("sim1", "matan", "ch1", "mc");
-		driver.addEdge("sim1", "ch1", "shir", "1s");
-		driver.addEdge("sim1", "ch3", "amit", "2a");
-		driver.addEdge("sim1", "ch2", "matan", "3s");
-		driver.addEdge("sim1", "shir", "ch4", "s4");
-		driver.addEdge("sim1", "ch4", "natali", "4n");
-
-
-
-		
-		//check balance
-		assertEquals(driver.getParticipantBalace("sim1", "amit") ,0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan"), 0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir"), 0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "natali"), 0,delta);
-
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-		assertEquals(driver.listContents("sim1", "ch3"),"");
-		assertEquals(driver.listContents("sim1", "ch4"),"");
-
-		
-		// create transactions
-		Transaction t1 = new Transaction("shir", 20);
-		Transaction t2 = new Transaction("natali", 20);
-
-		// send transaction
-		driver.sendTransaction("sim1", "ch2", t1);
-		driver.sendTransaction("sim1", "ch3", t2);
-		
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"20.0");
-		assertEquals(driver.listContents("sim1", "ch3"),"20.0");
-
-
-		//simulate
-		driver.simulate("sim1");
-		
-		
-		//check balance
-		assertEquals(driver.getParticipantBalace("sim1", "amit") , 5.0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan") , 5.0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir") , 0.0,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "natali") , 0.0,delta);
-
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"15.0 15.0");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-		assertEquals(driver.listContents("sim1", "ch3"),"");
-		assertEquals(driver.listContents("sim1", "ch4"),"");
-
-
-		
-		//simulate
-		driver.simulate("sim1");
-		
-		//check values
-		
-		//check balance
-		
-		assertEquals(driver.getParticipantBalace("sim1", "amit") , 5,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan") , 5,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir") , 20,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "natali") , 0.0,delta);
-
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-		assertEquals(driver.listContents("sim1", "ch3"),"");
-		assertEquals(driver.listContents("sim1", "ch4"),"10.0");
-
-		//simulate
-		driver.simulate("sim1");
-		
-		//check values
-		
-		//check balance
-		
-		assertEquals(driver.getParticipantBalace("sim1", "amit") , 5,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "matan") , 5,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "shir") , 20,delta);
-		assertEquals(driver.getParticipantBalace("sim1", "natali") , 10.0,delta);
-
-		//check contents
-		assertEquals(driver.listContents("sim1", "ch1"),"");
-		assertEquals(driver.listContents("sim1", "ch2"),"");
-		assertEquals(driver.listContents("sim1", "ch3"),"");
-		assertEquals(driver.listContents("sim1", "ch4"),"");
-		
-	}
-	
 }
